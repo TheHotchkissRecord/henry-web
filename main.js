8d2bfe81f888bc6f20345413dd9b35889200d813
reeceyang <reeceyang@live.com>
2020-11-24 19:12:43 -0600

    switch between articles and save their contents

--- main.js
+++ main.js
@@ -25,14 +25,23 @@ function findArticleById(theId) {
   return -1;
 }
 
+
 function listArticles() {
   listBox = document.getElementById("articles-list");
+  listBox.innerHTML = "";
   for (var i = 0; i < theNewsletter.articleOrder.length; i++) {
     articleP = document.createElement("p");
     console.log(findArticleById(theNewsletter.articleOrder[i]));
     theArticle = theNewsletter.articles[findArticleById(theNewsletter.articleOrder[i])];
     articleP.id = theArticle.id;
     articleP.appendChild(document.createTextNode((i + 1) + ". " + theArticle.title));
+
+    articleP.addEventListener("click", function (event){
+      console.log("clicked");
+      theArticleId = event.target.id;
+      fillFields();
+    });
+
     listBox.appendChild(articleP)
   }
 }
@@ -48,6 +57,25 @@ function fillFields() {
   document.getElementById("thumbnail-credit").value = theArticle.thumbnailCredit;
   document.getElementById("article-id").innerHTML = theArticleId;
 }
+
+function updateNewsletter() {
+  theNewsletter.articles[theArticleId].title = document.getElementById("title").value;
+  theNewsletter.articles[theArticleId].articleLink = document.getElementById("article-link").value;
+  theNewsletter.articles[theArticleId].byline = document.getElementById("byline").value;
+  theNewsletter.articles[theArticleId].contentPreview = document.getElementById("content-preview").value;
+  theNewsletter.articles[theArticleId].thumbnailCaption = document.getElementById("thumbnail-caption").value;
+  theNewsletter.articles[theArticleId].thumbnailLink = document.getElementById("thumbnail-link").value;
+  theNewsletter.articles[theArticleId].thumbnailCredit = document.getElementById("thumbnail-credit").value;
+}
+
+document.getElementById("title").addEventListener("input", updateNewsletter);
+document.getElementById("article-link").addEventListener("input", updateNewsletter);
+document.getElementById("byline").addEventListener("input", updateNewsletter);
+document.getElementById("content-preview").addEventListener("input", updateNewsletter);
+document.getElementById("thumbnail-caption").addEventListener("input", updateNewsletter);
+document.getElementById("thumbnail-link").addEventListener("input", updateNewsletter);
+document.getElementById("thumbnail-credit").addEventListener("input", updateNewsletter);
+document.getElementById("article-id").addEventListener("input", updateNewsletter);
 
 function fillAll() {
   listArticles();
